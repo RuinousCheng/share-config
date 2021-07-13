6ce7753f55f5e3f9e93e4d969d2616b9c55a931a
程瑞 <chengrui02@meituan.com>
2021-07-13 11:33:54 +0800

    docs: add comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-template-curly-in-string': 'error',
-    'no-unexpected-multiline': 'warn',
+    'no-template-curly-in-string': 'error',// 不允许常规字符串包含看起来像模板字面占位符的内容
+    'no-unexpected-multiline': 'warn',// 不允许混淆多行表达式
   },
 }
